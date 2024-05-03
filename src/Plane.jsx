d28f047d05d238521bb82a9985c71d67f1ebf193
Balzac <balzac.baudemont@gmail.com>
2024-05-03 15:15:33 +0200

    add cubes

--- src/Plane.jsx
+++ src/Plane.jsx
@@ -4,6 +4,8 @@ import * as THREE from 'three';
 function Plane() {
   const containerRef = useRef(null);
 
+  const DEFAULT_X_ROTATION = 5;
+
   useEffect(() => {
     if (!containerRef.current || containerRef.current.children.length !== 0) {
       return;
@@ -26,8 +28,21 @@ function Plane() {
     const planeGeometry = new THREE.PlaneGeometry(20, 20);
     const planeMaterial = new THREE.MeshBasicMaterial({ color: 'green', side: THREE.DoubleSide });
     const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-    plane.rotation.set(5, 0, 0);
+    plane.rotation.set(DEFAULT_X_ROTATION, 0, 0);
     scene.add(plane);
+
+    const cubeGeometry = new THREE.BoxGeometry(2, 2, 2);
+    const cubeMaterial = new THREE.MeshBasicMaterial({ color: "#00FFAA" });
+    const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    cube.rotation.set(DEFAULT_X_ROTATION, 0, 0);
+    scene.add(cube)
+
+    const cubeTwoGeometry = new THREE.BoxGeometry(2, 2, 2);
+    const cubeTwoMaterial = new THREE.MeshBasicMaterial({ color: "red" });
+    const cubeTwo = new THREE.Mesh(cubeTwoGeometry, cubeTwoMaterial);
+    cubeTwo.rotation.set(DEFAULT_X_ROTATION, 0, 0);
+    cubeTwo.position.set(7, 0, 7);
+    scene.add(cubeTwo)
 
     // Renderer
     const renderer = new THREE.WebGLRenderer({ antialize: true });
@@ -36,7 +51,15 @@ function Plane() {
     // JS to append domElement into our HTML 
     containerRef.current.appendChild(renderer.domElement);
 
-    renderer.render(scene, camera);
+
+    function animate() {
+      cube.rotation.z += 0.01
+      cubeTwo.rotation.z += 0.01
+      requestAnimationFrame(animate);
+      renderer.render(scene, camera);
+    }
+
+    animate();
   }, [containerRef]);
 
   return (
