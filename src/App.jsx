b6045f156feb04e3a7544ba2e4158e2d6240dfd3
Balzac <balzac.baudemont@gmail.com>
2024-05-03 10:13:26 +0200

    init + section 2

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useRef } from 'react';
+import * as THREE from 'three';
+
+function App() {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    if (!containerRef.current || containerRef.current.children.length !== 0) {
+      return;
+    }
+    
+    // Scene
+    const scene = new THREE.Scene();
+
+    // Camera
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    const viewAngle = 45;
+    const nearClipping = 0.1;
+    const farClipping = 9999;
+
+    const camera = new THREE.PerspectiveCamera(viewAngle, width / height, nearClipping, farClipping);
+    camera.position.set(0, 0, 10);
+
+    // Mesh
+    const cubeGeometry = new THREE.BoxGeometry(2, 2, 2);
+    const cubeMaterial = new THREE.MeshBasicMaterial({ color: "#00FFAA" });
+    const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    cube.position.set(0, 0, 0);
+    scene.add(cube)
+
+    // Renderer
+    const renderer = new THREE.WebGLRenderer({ antialize: true });
+    renderer.setSize(width, height);
+
+    // JS to append domElement into our HTML 
+    containerRef.current.appendChild(renderer.domElement);
+
+    function animate() {
+      cube.rotation.y += 0.01
+      cube.rotation.x += 0.01
+      requestAnimationFrame(animate);
+      renderer.render(scene, camera);
+    }
+
+    animate();
+  }, [containerRef]);
+
+  return (
+    <>
+      <div ref={containerRef} />
+    </>
+  )
+}
+
+export default App
